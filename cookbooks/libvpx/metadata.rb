2599edd0b2bb174640dd9c609f1a43e6f8756c27
SteenJobs <josh.e.siegel@gmail.com>
2016-11-16 13:59:00 -0500

    Import libvpx version 1.2.2

---
+++ cookbooks/libvpx/metadata.rb
@@ -0,0 +1,13 @@
+name 'libvpx'
+maintainer 'Escape Studios'
+maintainer_email 'dev@escapestudios.com'
+license 'MIT'
+description 'Installs/Configures libvpx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.2.2'
+
+supports 'ubuntu'
+
+depends 'git'
+depends 'build-essential'
+depends 'yasm'
