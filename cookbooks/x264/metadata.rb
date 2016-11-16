814cd1879116e1a7cfba40f6b9676bb7f2e63e93
SteenJobs <josh.e.siegel@gmail.com>
2016-11-16 13:57:54 -0500

    Import x264 version 0.6.0

---
+++ cookbooks/x264/metadata.rb
@@ -0,0 +1,13 @@
+name 'x264'
+maintainer 'Escape Studios'
+maintainer_email 'dev@escapestudios.com'
+license 'MIT'
+description 'Installs/Configures x264'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.6.0'
+
+supports 'ubuntu'
+
+depends 'git'
+depends 'build-essential'
+depends 'yasm'
