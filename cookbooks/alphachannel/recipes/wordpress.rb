b40728eb7de60d61a473b6eb69e42d2f0e94bfab
SteenJobs <josh.e.siegel@gmail.com>
2016-12-01 14:12:53 -0500

    update wordpress recipe

--- cookbooks/alphachannel/recipes/wordpress.rb
+++ cookbooks/alphachannel/recipes/wordpress.rb
@@ -3,9 +3,9 @@ node.override["wordpress"]["db"]["root_password"] = mysqlpass["password"]
 node.override["wordpress"]["parent_dir"] = "/home/#{node["alphachannel"]["user"]}/apps"
 node.override['wordpress']['dir'] = "/home/#{node["alphachannel"]["user"]}/apps/wordpress/blog"
 node.override['wordpress']['db']['pass'] = "password"
-#node.override['apache']['default_site_port'] = '8080'
-#node.override['wordpress']['server_port'] = '8080'
-#node.override['apache']['listen']            = ['*:8080']
+node.override['apache']['default_site_port'] = '8080'
+node.override['wordpress']['server_port'] = '8080'
+node.override['apache']['listen']            = ['*:8080']
 #node.override['wordpress']['install']['user'] = node["alphachannel"]["user"]
 
 execute "sudo apt-get install php5-fpm php5-mysql -y"
