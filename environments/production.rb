61eb0ede712cd845300f82d0dd4df9c59235b8ff
SteenJobs <josh.e.siegel@gmail.com>
2016-12-01 16:35:51 -0500

    create prod environment

---
+++ environments/production.rb
@@ -0,0 +1,3 @@
+name "production"
+description "The master production branch"
+default_attributes "alphachannel" => { "db" => { "host" => "ampsju0945wrcq.chwwxm9ckx0l.us-west-2.rds.amazonaws.com:5432", "name" => "ac_prod", "username" => "deploy", "port" => "5432", "password" => "password" } }
